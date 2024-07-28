806789f597140801a108a09384cfa0cb66c50354
AchrafYndz <achraf.yandouzi@gmail.com>
2024-07-28 01:00:27 +0100

    Fix main.py setup

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-from strong_to_gymrun import strong_to_gymrun
+# from strong_to_gymrun import strong_to_gymrun
 from gymrun_to_hevy import gymrun_to_hevy
-from fix_double import fix_double
-
 
 def main():
-    fix_double()
+    # strong_to_gymrun()
     gymrun_to_hevy()
 
 
